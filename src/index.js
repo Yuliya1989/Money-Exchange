5b48701dc67a4b48bdf91ecda8726dcd4f163c69
Sinkevich Yuliya <yuliya.sinkevich1@gmail.com>
2018-02-27 12:50:42 +0300

    fifth task

--- src/index.js
+++ src/index.js
@@ -2,4 +2,33 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+var obj = {};
+
+var valH=Math.floor(currency/50);
+var valQ=Math.floor((currency-valH*50)/25);
+var valD=Math.floor(((currency-valH*50)-25*valQ)/10);
+var valN=Math.floor((((currency-valH*50)-25*valQ)-10*valD)/5);
+var valP=(((currency-valH*50)-25*valQ)-10*valD)-5*valN;
+
+if (currency>10000) {
+obj["error"]="You are rich, my friend! We don't have so much coins for exchange";
+} else {
+if (valH>0) {
+obj["H"]=valH;
+}
+if (valQ>0) {
+obj["Q"]=valQ;
+}
+if (valD>0) {
+obj["D"]=valD;
+}
+if (valN>0) {
+obj["N"]=valN;
+}
+if (valP>0) {
+obj["P"]=valP;
+}
+}
+
+return obj;
 }
